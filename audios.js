cd5c0fdb334dc5b477c843a12b06650dfd121464
Saiperi <saiperi@paypal.com>
2026-01-04 09:29:35 +0530

    audiosearch

--- audios.js
+++ audios.js
@@ -1,5 +1,5 @@
 const bhajanAudios = [
-  { date: "2025-12-28", soundcloudUrl: "https://soundcloud.com/sai-karthik-171032973/28th-dec-2025-2?in=sai-karthik-171032973/sets/bhajans&si=ec753445d51f4933a6e0157e98f25fa8", label: "Sunday Bhajans" },
-//   { date: "2026-01-01", soundcloudUrl: "https://soundcloud.com/your-account/01st-jan-2026", label: "Thursday Bhajans" },
-//   { date: "2025-11-30", soundcloudUrl: "https://soundcloud.com/your-account/30th-nov-2025", label: "Sunday Bhajans" }
+  { date: "2025-12-28", soundcloudUrl: "https://soundcloud.com/sai-karthik-171032973/28th-dec-2025-2", label: "Sunday Bhajans" },
+  { date: "2026-01-01", soundcloudUrl: "https://soundcloud.com/sai-karthik-171032973/01stjan2026-1-1", label: "Thursday Bhajans" },
+  { date: "2025-11-30", soundcloudUrl: "https://soundcloud.com/sai-karthik-171032973/30thnov2025-2", label: "Sunday Bhajans" }
 ];
