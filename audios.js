ea44ff4b5488bc57358e4b3d537248c9f84a2a0d
Saiperi <saiperi@paypal.com>
2026-01-04 10:08:54 +0530

    Replace 28th dec audio file

--- audios.js
+++ audios.js
@@ -1,6 +1,6 @@
 // Local audio files - plays directly in browser
 const bhajanAudios = [
-  { date: "2025-12-28", audioFile: "audios/28th+December.mp3", label: "Sunday Bhajans" },
+  { date: "2025-12-28", audioFile: "audios/28th December.mp3", label: "Sunday Bhajans" },
   { date: "2026-01-01", audioFile: "audios/01st+Jan+2026 (1).mp3", label: "Thursday Bhajans" },
   { date: "2025-11-30", audioFile: "audios/30th+Nov+2025.mp3", label: "Sunday Bhajans" }
 ];
