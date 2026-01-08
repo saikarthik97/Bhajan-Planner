c294b6f27c6e7a3edf4e549cd64ff380088fe319
Saiperi <saiperi@paypal.com>
2026-01-09 02:46:16 +0530

    Added 8th Jan audio and updated bhajan files

--- audios.js
+++ audios.js
@@ -4,4 +4,5 @@ const bhajanAudios = [
   { date: "2026-01-01", audioFile: "audios/01st Jan 2026.mp3", label: "Thursday Bhajans" },
   { date: "2025-11-30", audioFile: "audios/30th+Nov+2025.mp3", label: "Sunday Bhajans" },
   { date: "2026-01-04", audioFile: "audios/4th Jan 2026.mp3", label: "Sunday Bhajans" },
+  { date: '2026-01-08',audioFile: "audios/8th Jan.mp3",label: "Thursday Bhajans" },
 ];
